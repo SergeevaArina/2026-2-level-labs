bc7de330dbbe49f0d232839f58897f74d805ccf3
Arina <arineeee2005@gmail.com>
2026-09-23 19:58:12 +0300

    bbbbb

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -234,6 +234,12 @@ def compare_profiles_by_top_n(
     top_n_to_compare = get_top_n_words(profile_to_compare[1], top_n)
     common_tokens = [word for word in top_n_unknown if word in top_n_to_compare]
 
+    if not all([
+        isinstance(top_n_unknown, list),
+        isinstance(top_n_to_compare, list)
+    ]):
+        return None
+
     if not common_tokens:
         common_tokens_numb = 0
     common_tokens_numb = len(common_tokens)
